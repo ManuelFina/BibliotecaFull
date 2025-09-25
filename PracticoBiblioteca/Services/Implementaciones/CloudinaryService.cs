241ed0d8e4ed3c67bd47f4f8e80b9a2a18996cfd
Manuel Fina <finamanuelmateo@gmail.com>
2025-09-25 20:00:51 -0300

    Se agrego Cloudinary al FrontEnd

---
+++ PracticoBiblioteca/Services/Implementaciones/CloudinaryService.cs
@@ -0,0 +1,33 @@
+﻿using PracticoBiblioteca.Shared.DTOs;
+using PracticoBiblioteca.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace PracticoBiblioteca.Services.Implementaciones;
+
+public class CloudinaryService : ICloudinaryService
+{
+    private readonly HttpClient _httpClient;
+
+    public CloudinaryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<CloudinaryResponseDTO?> SubirImagen(Stream fileStream, string fileName)
+    {
+        try
+        {
+            using var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(fileStream), "file", fileName);
+
+            var response = await _httpClient.PostAsync("api/upload/image", content);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<CloudinaryResponseDTO>();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error al subir la imagen a Cloudinary", ex);
+        }
+    }
+}
---
+++ PracticoBiblioteca/Services/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,15 @@
+﻿using PracticoBiblioteca.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticoBiblioteca.Services.Interfaces
+{
+    public interface ICloudinaryService
+    {
+        Task<CloudinaryResponseDTO?> SubirImagen(Stream fileStream, string fileName);
+
+    }
+}
