c68040b3f3092ae908d5acb9d01a4092fa426e86
Manuel Fina <finamanuelmateo@gmail.com>
2025-09-25 21:23:46 -0300

    Logica de prestamos

---
+++ PracticoBiblioteca.API/Repositories/Interfaces/IPrestamoRepository.cs
@@ -0,0 +1,13 @@
+﻿using PracticoBiblioteca.Shared.Models;
+
+namespace PracticoBiblioteca.API.Repositories.Interfaces
+{
+    public interface IPrestamoRepository
+    {
+        Task<Prestamo> CrearAsync(Prestamo prestamo);
+        Task<List<Prestamo>> ObtenerTodosAsync();
+        Task<Prestamo?> ObtenerPorIdAsync(int id);
+        Task<List<Prestamo>> ObtenerPorUsuarioAsync(int usuarioId);
+        Task DevolverAsync(int prestamoId);
+    }
+}
---
+++ PracticoBiblioteca.Shared/Models/Prestamo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PracticoBiblioteca.Shared.Models
+{
+    public class Prestamo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+
+        [Required]
+        public int LibroId { get; set; }
+        public Libro? Libro { get; set; }
+
+        [Required]
+        public DateTime FechaPrestamo { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime FechaDevolucion { get; set; }
+
+        public bool Devuelto { get; set; } = false;
+    }
+}
